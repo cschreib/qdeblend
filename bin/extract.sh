5468a2d631ba1a4219186fa80c53193756738a4d
Corentin Schreiber <cschreib@orange.fr>
2019-07-03 10:02:31 +0200

    Fixed software directory assumed to be current directory

--- bin/extract.sh
+++ bin/extract.sh
@@ -18,6 +18,9 @@ echo ${IMG}
 
 DIR=`dirname ${IMG}`
 
+# Change here the directory where the software is located (default: current directory)
+BIN_DIR=.
+
 if [ ${LARGE_SCALE_FILTER} -gt 0 ]; then
     # Backup the original image so we don't apply twice
     if [ -f ${IMG}-orig.fits ]; then
@@ -26,32 +29,32 @@ if [ ${LARGE_SCALE_FILTER} -gt 0 ]; then
         cp ${IMG}.fits ${IMG}-orig.fits
     fi
 
-    ovif ./make_mask ${IMG}.fits ${DIR}/mask_all.reg ${IMG}-mask-all.fits
+    ${BIN_DIR}/make_mask ${IMG}.fits ${DIR}/mask_all.reg ${IMG}-mask-all.fits
     if [ -f ${IMG}-bad.reg ]; then
-        ovif ./add_mask ${IMG}-mask-all.fits ${IMG}-bad.reg
+        ${BIN_DIR}/add_mask ${IMG}-mask-all.fits ${IMG}-bad.reg
     fi
 
-    ovif ./large_scale_filter image=${IMG}.fits size=${LARGE_SCALE_FILTER} mask=${IMG}-mask-all.fits
+    ${BIN_DIR}/large_scale_filter image=${IMG}.fits size=${LARGE_SCALE_FILTER} mask=${IMG}-mask-all.fits
 fi
 
 if [ ${MASK_MORE} -eq 0 ]; then
-    ovif ./make_mask ${IMG}.fits ${DIR}/mask_border.reg ${IMG}-mask-border.fits
+    ${BIN_DIR}/make_mask ${IMG}.fits ${DIR}/mask_border.reg ${IMG}-mask-border.fits
 fi
 if [ ${MASK_MORE} -eq 1 ]; then
-    ovif ./make_mask ${IMG}.fits ${DIR}/mask_border_more.reg ${IMG}-mask-border.fits
+    ${BIN_DIR}/make_mask ${IMG}.fits ${DIR}/mask_border_more.reg ${IMG}-mask-border.fits
 fi
 
 if [ -f ${IMG}-bad.reg ]; then
-    ovif ./add_mask ${IMG}-mask-border.fits ${IMG}-bad.reg
+    ${BIN_DIR}/add_mask ${IMG}-mask-border.fits ${IMG}-bad.reg
 fi
 
 if [ ${GET_SHIFT} -eq 0 ]; then
-    ovif ./multi_subtract image=${IMG}.fits psf=${DIR}/${PSF} oversample=${OVERSAMPLE} \
+    ${BIN_DIR}/multi_subtract image=${IMG}.fits psf=${DIR}/${PSF} oversample=${OVERSAMPLE} \
         mask=${IMG}-mask-border.fits out=${IMG}-allsub.fits error=${ERROR} \
         model_dir=${DIR}/imfit/models xshift=${XSHIFT} yshift=${YSHIFT} reuse_models=0 \
         ra_center=${RAC} dec_center=${DECC}
 else
-    ovif ./multi_subtract image=${IMG}.fits psf=${DIR}/${PSF} oversample=${OVERSAMPLE} \
+    ${BIN_DIR}/multi_subtract image=${IMG}.fits psf=${DIR}/${PSF} oversample=${OVERSAMPLE} \
         mask=${IMG}-mask-border.fits out=${IMG}-allsub.fits error=${ERROR} \
         model_dir=${DIR}/imfit/models shift_anchors=${SHIFT_ANCHOR} \
         shift_fit_radius=${SHIFT_MASK} max_shift=${SHIFT_MAX} shift_dp=${SHIFT_STEP} \
